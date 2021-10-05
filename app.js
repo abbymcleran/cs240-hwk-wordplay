158af9f136ef631d6948f9361e993a799117e386
Abby McLeran <amcleran@pugetsound.edu>
2021-10-05 14:54:10 -0700

    getting closer...

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 // randomly pick 6 letter word from dictionary
-let word = Math.random()*61337;
+let wordVal = Math.floor(Math.random()*61337);
+let word = dictionary[wordVal];
+if(word.length != 6){
 while (word.length != 6){
-    word = Math.random()*61337;
+    wordVal = Math.floor(Math.random()*61337);
+    word = dictionary[wordVal];
+}
 }
 
 // hold the letters of the word in an array
@@ -13,21 +17,46 @@ for (let i=0; i<6; i++){
 // array for holding the different word combinations
 let unguessed = [];
 
-// function to be used for checking if a letter combination is a word
-function wordCheck(a, b, c, d, e, f) {
-    let tempWord = a+b+c+d+e+f;
-    if ('words_alpha.js'.dictionary.contains(tempWord)){
-        unguessed.push(tempWord)
+// functions to be used for checking if a letter combination is a word
+function wordCheck3(a, b, c) {
+    let tempWord = a+b+c;
+    if (!unguessed.includes(tempWord)){
+        if (dictionary.includes(tempWord)){
+            unguessed.push(tempWord)
+        }
     }
 }
+function wordCheck4(a, b, c, d) {
+    let tempWord = a+b+c+d;
+    if (!unguessed.includes(tempWord)){
+        if (dictionary.includes(tempWord)){
+            unguessed.push(tempWord)
+        }
+    }
+}
+function wordCheck5(a, b, c, d, e) {
+    let tempWord = a+b+c+d+e;
+    if (!unguessed.includes(tempWord)){
+        if (dictionary.includes(tempWord)){
+            unguessed.push(tempWord)
+        }
+    }
+}
+function wordCheck6(a, b, c, d, e, f) {
+    let tempWord = a+b+c+d+e+f;
+    if (!unguessed.includes(tempWord)){
+        if (dictionary.includes(tempWord)){
+            unguessed.push(tempWord)
+        }
+    }
+}
+    
 
 // checking for 3-letter words from letter combinations
 for(let a=0; a<6; a++){
     for(let b=0; b<6; b++){
         for(let c=0; c<6; c++){
-            if(a!=b && b!=c && a!=c) {
-                wordCheck(letters[a], letters[b], letters[c]);
-            }
+                wordCheck3(letters[a], letters[b], letters[c]);
         }
     }
 }
@@ -36,9 +65,7 @@ for(let a=0; a<6; a++){
     for(let b=0; b<6; b++){
         for(let c=0; c<6; c++){
             for(let d=0; d<6; d++){
-                if(a!=b && b!=c && a!=c && a!=d && b!=d && c!=d) {
-                    wordCheck(letters[a], letters[b], letters[c], letters[d]);
-                }
+                    wordCheck4(letters[a], letters[b], letters[c], letters[d]);
             }
         }
     }
@@ -49,9 +76,7 @@ for(let a=0; a<6; a++){
         for(let c=0; c<6; c++){
             for(let d=0; d<6; d++){
                 for(let e=0; e<6; e++){
-                    if(a!=b && b!=c && a!=c && a!=d && b!=d && c!=d && a!=e && b!=e && c!=e && d!=e) {
-                        wordCheck(letters[a], letters[b], letters[c], letters[d], letters[e]);
-                    }
+                        wordCheck5(letters[a], letters[b], letters[c], letters[d], letters[e]);
                 }
             }
         }
@@ -64,9 +89,7 @@ for(let a=0; a<6; a++){
             for(let d=0; d<6; d++){
                 for(let e=0; e<6; e++){
                     for(let f=0; f<6; f++){
-                        if(a!=b && b!=c && a!=c && a!=d && b!=d && c!=d && a!=e && b!=e && c!=e && d!=e && a!=f && b!=f && c!=f && d!=f && e!=f) {
-                            wordCheck(letters[a], letters[b], letters[c], letters[d], letters[e], letters[f]);
-                        }
+                            wordCheck6(letters[a], letters[b], letters[c], letters[d], letters[e], letters[f]);
                     }
                 }
             }
@@ -79,24 +102,30 @@ let scramble = [];
 
 // function for scrambling letters
 function letterScramble() {
-    while (scramble.length < 6){
-        temp = letters[Math.random()*6];
-        if (!scramble.contains(temp)){
-            scramble.push(temp);
+    let temp =[];
+    for (let i=0; i<letters.length; i++){
+        temp.push(letters[i]);
+    }
+    for (let i=0; i<letters.length; i++){
+        let randIndex = Math.floor(Math.random()*(temp.length));
+            scramble[i] = temp.splice(randIndex, 1);
         }
     }
-}
 
 // make a call to scramble the letters
 letterScramble();
+
+// array to hold the correctly guessed words
+let guessed = [];
 
 // make call for user input and print scrambled letters and blank lines
 // function for outputting the scrambled letters as well as the blank spaces for
 // possible words; words that have been guessed will show up
 function outputInfo() {
-    console.log(scramble);
+    console.clear();
+    console.log(scramble[0]+" "+scramble[1]+" "+scramble[2]+" "+scramble[3]+" "+scramble[4]+" "+scramble[5]);
     for(let i = 0; i < unguessed.length; i++) {
-        if(guessed.contains(unguessed[i])) {
+        if(guessed.includes(unguessed[i])) {
             console.log(unguessed[i]);
         }
         else {
@@ -121,9 +150,6 @@ function outputInfo() {
 outputInfo();
 let input = prompt("Enter a guess:");
 
-// array to hold the correctly guessed words
-let guessed = [];
-
 // while the user is playing the game, this will check the inputs given
 while (guessed.length!=unguessed.length) {
     outputInfo();
@@ -131,13 +157,13 @@ while (guessed.length!=unguessed.length) {
     if (input == null){
         endOfGame();
     }
-    else if (input == "*"){
+    if (input == "*"){
         letterScramble();
     }
-    else if (guessed.contains(input)){
+    else if (guessed.includes(input)){
         alert("Already guessed " + input + "!");
     }
-    else if (unguessed.contains(input)){
+    else if (unguessed.includes(input)){
         alert("Correct! " + input);
         guessed.push(input);
     }
